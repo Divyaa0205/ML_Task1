7e89166ec9f6ab855955e0dbe9453c9ed2f4d02d
Divyaa0205 <divya2310084@akgec.ac.in>
2024-10-14 22:43:57 +0530

    Winning_cases function created

--- play.py
+++ play.py
@@ -3,4 +3,15 @@
 import random
 import sys
 
-choices = {"rock": "scissors", "paper": "rock", "scissors": "paper"}
+choices = ["rock", "paper", "scissors"]
+win_cases = {"rock": "scissors", "paper": "rock", "scissors": "paper"}
+
+def winning(user, computer):
+    if computer == user:
+        return "Draw"
+    elif(user, computer) in win_cases:
+        return "User Wins!"
+    else:
+        return "Computer Wins"
+
+def play
